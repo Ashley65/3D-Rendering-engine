ced911d574dfa828f4cb77fc3e8f99208fa9477e
Ashley65 <nchinembiri@outlook.com>
2023-03-04 00:54:38 +0000

    22004221

---
+++ src/Panel.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.*;
+
+public class Panel extends JFrame implements Runnable{
+
+
+    public Panel() {
+
+    }
+}
